a3018a89b623e6042b9ec8eac54081ee2a12ab8b
Anddrrek <andrei.fc40@gmail.com>
2019-03-03 14:38:27 +0300

    added GrossyUIKitDemo app

---
+++ Grossy/Grossy_UIKit_Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Grossy_UIKit_Demo
+//
+//  Created by Andrey on 03/03/2019.
+//  Copyright © 2019 anddrrek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Grossy/Grossy_UIKit_Demo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Grossy_UIKit_Demo
+//
+//  Created by Andrey on 03/03/2019.
+//  Copyright © 2019 anddrrek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
