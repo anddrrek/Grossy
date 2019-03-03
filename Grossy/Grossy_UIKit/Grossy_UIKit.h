f73ef1da7d07da19891646f452539e8a9f37410c
Anddrrek <andrei.fc40@gmail.com>
2019-03-03 14:37:06 +0300

    added GrossyUIKit framework

---
+++ Grossy/Grossy_UIKit/Grossy_UIKit.h
@@ -0,0 +1,19 @@
+//
+//  Grossy_UIKit.h
+//  Grossy_UIKit
+//
+//  Created by Andrey on 03/03/2019.
+//  Copyright © 2019 anddrrek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Grossy_UIKit.
+FOUNDATION_EXPORT double Grossy_UIKitVersionNumber;
+
+//! Project version string for Grossy_UIKit.
+FOUNDATION_EXPORT const unsigned char Grossy_UIKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Grossy_UIKit/PublicHeader.h>
+
+
