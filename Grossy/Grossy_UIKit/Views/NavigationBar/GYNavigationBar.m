59b7bc79a05fbb00eaa39d365d792321f5352e76
Anddrrek <andrei.fc40@gmail.com>
2019-03-03 15:33:53 +0300

    added base and xib for navigation bar

--- Grossy/Grossy_UIKit/Views/NavigationBar/GYNavigationBar.m
+++ Grossy/Grossy_UIKit/Views/NavigationBar/GYNavigationBar.m
@@ -12,17 +12,50 @@
 
 @property (assign, nonatomic) GYNavigationBarStyle style;
 
+@property (weak, nonatomic) IBOutlet UIView *view;
+@property (weak, nonatomic) IBOutlet UIButton *searchButton;
+@property (weak, nonatomic) IBOutlet UILabel *titleLabel;
+
 @end
 
 @implementation GYNavigationBar
 
+#pragma mark - Initialization
+
+- (instancetype)initWithStyle:(GYNavigationBarStyle)style {
+    CGRect defaultFrame = CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, 130);
+    if (self = [self initWithFrame:defaultFrame]) {
+
+    }
+    return self;
+}
+
+- (instancetype)initWithFrame:(CGRect)frame {
+    if (self = [super initWithFrame:frame]) {
+        [self commonInit];
+    }
+    return self;
+}
+
+- (instancetype)initWithCoder:(NSCoder *)aDecoder {
+    if (self = [super initWithCoder:aDecoder]) {
+        [self commonInit];
+    }
+    return  self;
+}
+
+
+- (void)commonInit {
+
+}
+
 #pragma mark - Accessors
 - (void)setTitle:(NSString *)title {
-    
+    _titleLabel.text = title;
 }
 
 - (NSString *)title {
-    return @"";
+    return _titleLabel.text;
 }
 
 @end
